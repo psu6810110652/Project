ca4d586fdcedc54adbf03b8aa1ffabbd1bfe5c09
Teerayut <6810110162@psu.ac.th>
2026-02-22 22:38:14 +0700

    fix:product.controller

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,43 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+
+
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../auth/roles.decorator'; 
+import { RolesGuard } from '../auth/roles.guard';
+import { UserRole } from '../users/entities/user.entity';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
+  
   @Post()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  
   @Get('promotions')
   findPromotions() {
     return this.productService.findPromotions();
   }
 
+  
   @Get()
   findAll() {
     return this.productService.findAll();
   }
 
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
+  
   @Get('category/:categoryId')
   async findByCategory(@Param('categoryId') categoryId: string) {
     return this.productService.findAllByCategory(+categoryId);
   }
 
+  
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
+  
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
   }
