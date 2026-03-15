0595303864c61d748f8eafe19fb613495880eab2
Teerayut <6810110162@psu.ac.th>
2026-03-15 20:11:28 +0700

    fix:PDPA Backend

--- backend/src/review/entities/review.entity.ts
+++ backend/src/review/entities/review.entity.ts
@@ -24,14 +24,12 @@ export class Review {
     @UpdateDateColumn()
     updatedAt: Date;
 
-<<<<<<< HEAD
-    @Column({nullable: true})
-=======
-    @Column({ type: 'decimal', precision: 3, scale: 1, default: 0, transformer: {
-        to: (value: number) => value,
-        from: (value: string) => parseFloat(value)
-    } })
->>>>>>> main
+    @Column({
+        type: 'decimal', precision: 3, scale: 1, default: 0, transformer: {
+            to: (value: number) => value,
+            from: (value: string) => parseFloat(value)
+        }
+    })
     rating: number;
 
     @Column('text')
