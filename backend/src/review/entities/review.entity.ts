b94cc925852e2e121289f7201e2fe95caced4df7
psu6810110406 <6810110406@psu.ac.th>
2026-03-15 20:44:28 +0700

    review

--- backend/src/review/entities/review.entity.ts
+++ backend/src/review/entities/review.entity.ts
@@ -24,14 +24,10 @@ export class Review {
     @UpdateDateColumn()
     updatedAt: Date;
 
-<<<<<<< HEAD
-    @Column({nullable: true})
-=======
     @Column({ type: 'decimal', precision: 3, scale: 1, default: 0, transformer: {
         to: (value: number) => value,
         from: (value: string) => parseFloat(value)
     } })
->>>>>>> main
     rating: number;
 
     @Column('text')
